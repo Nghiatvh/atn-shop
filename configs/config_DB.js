c8523c648ab1b08f7dd8bed25675f6c87827343c
nghiatvh <nghia17042001@gmail.com>
2020-11-06 17:30:41 +0700

    update

--- configs/config_DB.js
+++ configs/config_DB.js
@@ -1,16 +1,16 @@
 module.exports = {
     localdb: {
-        urldb : 'mongodb://localhost:27017/ATN',
-        dbname : "ATN",
-        dbusername : "",
-        dbpassword : "",
+        urldb : 'mongodb+srv://nghiatvh:nghia123456@cluster0.c0xty.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        dbname : "ATN-SHOP",
+        dbusername : "nghiatvh",
+        dbpassword : "nghia123456",
     },
 
     clouddb: {
-        urldb : 'mongodb://localhost:27017/ATN',
-        dbname : "ATN",
-        dbusername : "",
-        dbpassword : "",
+        urldb : 'mongodb+srv://nghiatvh:nghia123456@cluster0.c0xty.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        dbname : "ATN-SHOP",
+        dbusername : "nghiatvh",
+        dbpassword : "nghia123456",
     }
 
 };
